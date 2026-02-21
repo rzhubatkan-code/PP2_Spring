44501faa3da19af2cc1cdc43b8c57cb089683620
rzhubatkan-code <ruslanzhuabtkan@gmail.com>
2026-02-21 11:11:28 +0500

    Complete Practice 3: Python functions, lambda, and inheritance examples

---
+++ Practice3/classes/class_variables.py
@@ -0,0 +1,23 @@
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print("Hello!")
+
+p1 = Person("Emil")
+
+del Person.greet
+
+p1.greet() # This will cause an error
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name} ({self.age})"
+
+p1 = Person("Tobias", 36)
+print(p1)
